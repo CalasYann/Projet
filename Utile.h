6e210325ff6efd867b3260263db87e72422cf10c
CalasYann <133207210+CalasYann@users.noreply.github.com>
2023-05-27 16:01:07 +0200

    Create Utile.h

---
+++ Utile.h
@@ -0,0 +1,12 @@
+#ifndef UTILE_H
+  #define UTILE_H
+
+  #include <stdlib.h>
+  #include <stdio.h>
+  #include<unistd.h>
+
+  void Poubelle();
+  void Continuer();
+  int tourSuivant();
+  void Attendre();
+#endif

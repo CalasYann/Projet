94ed4880cde142d9f0beb0d7d0600147b90412c7
CalasYann <133207210+CalasYann@users.noreply.github.com>
2023-05-27 16:00:28 +0200

    Create Menu.h

---
+++ Menu.h
@@ -0,0 +1,12 @@
+#ifndef MENU_H
+  #define MENU_H
+
+  #include <stdlib.h>
+  #include <stdio.h>
+  #include  "Utile.h"
+  #include "Afficher.h"
+  #include "Fichier.h"
+
+  int Menu();
+  void Parametre();
+#endif
